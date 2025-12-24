b7a35259cc6a016007ab0f32a98336119c4297aa
vophuonganh <phuonganhdth123@gmail.com>
2025-12-24 13:25:42 +0700

    them controllers, models va views cho home

---
+++ webbanpizza/webbanpizza/Models/PhuongThucThanhToan.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace WebBanPizza.Models;
+
+public partial class PhuongThucThanhToan
+{
+    [Key]
+    public int PhuongThucId { get; set; }
+
+    public string? TenPhuongThuc { get; set; }
+
+    public virtual ICollection<DonHang> DonHangs { get; set; } = new List<DonHang>();
+}
