b221eeaed6495b1f53c8ad8ea0c63d50cc93a4d0
Baljeat_Les <hlan9693@gmail.com>
2025-12-24 15:44:46 +0700

    bo sung

--- webbanpizza/webbanpizza/obj/Debug/net8.0/webbanpizza.AssemblyInfo.cs
+++ webbanpizza/webbanpizza/obj/Debug/net8.0/webbanpizza.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webbanpizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcd2b6a6df9f5736c715529cb6f64c1c6c28d5d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d654289c364251bf1d63b44a295bd5c3f6f5a027")]
 [assembly: System.Reflection.AssemblyProductAttribute("webbanpizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webbanpizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
