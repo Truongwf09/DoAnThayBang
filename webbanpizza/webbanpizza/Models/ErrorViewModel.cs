2fb38bd80cbcef8462397c849093ae6205a47b1e
Baljeat_Les <hlan9693@gmail.com>
2025-12-24 13:18:40 +0700

    Them controllers, models, views cho feature/DonHang

--- webbanpizza/webbanpizza/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace webbanpizza.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
